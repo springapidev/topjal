aecc1281fedb3fb0f8d502ae470a25b3a0e8a4a2
Mohammad Rajaul Islam <springapidev@gmail.com>
2018-11-10 18:59:10 +0600

    link...

--- src/main/java/com/topjal/controller/ErrorController.java
+++ src/main/java/com/topjal/controller/ErrorController.java
@@ -1,46 +1,27 @@
 package com.topjal.controller;
 
+import com.topjal.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class ErrorController {
 
     @RequestMapping(value = "/error", method = RequestMethod.GET)
-    public ModelAndView renderErrorPage(HttpServletRequest httpRequest) {
-
-        ModelAndView errorPage = new ModelAndView("error");
-        String errorMsg = "";
-        int httpErrorCode = getErrorCode(httpRequest);
-
-        switch (httpErrorCode) {
-            case 400: {
-                errorMsg = "Http Error Code: 400. Bad Request";
-                break;
-            }
-            case 401: {
-                errorMsg = "Http Error Code: 401. Unauthorized";
-                break;
-            }
-            case 404: {
-                errorMsg = "Http Error Code: 404. Resource not found";
-                break;
-            }
-            case 500: {
-                errorMsg = "Http Error Code: 500. Internal Server Error";
-                break;
-            }
-        }
-        errorPage.addObject("errorMsg", errorMsg);
-        return errorPage;
+    public String renderErrorPage(HttpServletRequest request, HttpServletResponse response, Model model) {
+        int code = response.getStatus();
+        String message = HttpStatus.valueOf(code).getReasonPhrase();
+        model.addAttribute("code", code);
+        model.addAttribute("message", message);
+        return "error";
     }
 
-    private int getErrorCode(HttpServletRequest httpRequest) {
-        return (Integer) httpRequest
-                .getAttribute("javax.servlet.error.status_code");
-    }
+
 }
