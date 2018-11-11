8a793e06998730470f755c3fc8bc9ac212d6b01d
Mohammad Rajaul Islam <springapidev@gmail.com>
2018-11-11 18:43:13 +0600

    up........

--- src/main/java/com/topjal/controller/ErrorController.java
+++ src/main/java/com/topjal/controller/ErrorController.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,6 +17,7 @@ public class ErrorController {
     @RequestMapping(value = "/error", method = RequestMethod.GET)
     public String renderErrorPage(HttpServletRequest request, HttpServletResponse response, Model model) {
         int code = response.getStatus();
+        System.out.println("code::::: "+code);
         String message = HttpStatus.valueOf(code).getReasonPhrase();
         model.addAttribute("code", code);
         model.addAttribute("message", message);
